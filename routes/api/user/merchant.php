11fcabfa4a776907fe11c77590d24fc282dae7fb
hakim-asrori <saneglos005@gmail.com>
2022-11-26 13:59:38 +0700

    add feature show and update status

--- routes/api/user/merchant.php
+++ routes/api/user/merchant.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->prefix('user/merchant')->group(function () {
     Route::get('/', [MerchantController::class, 'index'])->name('api.user.merchant.index');
     Route::post('/', [MerchantController::class, 'store'])->name('api.user.merchant.store');
+    Route::put('/update-status/{id}', [MerchantController::class, 'updateStatus'])->name('api.user.merchant.update.status');
     Route::get('/{id}', [MerchantController::class, 'show'])->name('api.user.merchant.show');
     Route::put('/{id}', [MerchantController::class, 'update'])->name('api.user.merchant.update');
     Route::delete('/{id}', [MerchantController::class, 'delete'])->name('api.user.merchant.delete');
